bf9778dd133f6cff95ea7f6b1928ef5b90a575e9
Khalid Saifullah <khalid@outlook.com.au>
2024-04-24 14:44:42 +1000

    update cmd

--- list-bucket.sh
+++ list-bucket.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-AccessToken=$(gcloud auth print-identity-token)
-Response=$(aws sts assume-role-with-web-identity --role-arn "${AWS_ROLE_ARN}" --role-session "cloud-guild" --web-identity-token "$AccessToken")
+set -euo pipefail
+
+echo $AWS_ROLE_ARN
+export AccessToken=$(gcloud auth print-identity-token)
+export Response=$(aws sts assume-role-with-web-identity --role-arn "${AWS_ROLE_ARN}" --role-session "cloud-guild" --web-identity-token "$AccessToken")
 export AWS_ACCESS_KEY_ID=$(echo $Response | jq -r .Credentials.AccessKeyId)
 export AWS_SECRET_ACCESS_KEY=$(echo $Response | jq -r .Credentials.SecretAccessKey)
 export AWS_SESSION_TOKEN=$(echo $Response | jq -r .Credentials.SessionToken)
